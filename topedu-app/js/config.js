dcba1305edb74d0d25dab9308fed2f8641fd03cc
xiaohuichi <xiaohui.chi.nz@gmail.com>
2026-03-14 22:55:07 +1300

    frontend config.js

--- topedu-app/js/config.js
+++ topedu-app/js/config.js
@@ -2,8 +2,8 @@
     "use strict";
 
     window.APP_CONFIG = {
-        // API_BASE_URL: "https://topedu-chi.vercel.app/api",
-        API_BASE_URL: "http://localhost:3000/api",
+        API_BASE_URL: "https://topedu-25.onrender.com/api",
+        // API_BASE_URL: "http://localhost:3000/api",
         ACCESS_TOKEN_KEY: "topedu.accessToken",
         USER_KEY: "topedu.currentUser"
     };
